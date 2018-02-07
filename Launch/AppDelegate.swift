9f2276262969d6b684647697a81f487f3f2e9b12
super-amazing-joey-boy <joseph.slomowitz@gmail.com>
2018-02-07 16:47:22 +1100

    Cloned project -Added some AppDelgate methods

--- Launch/AppDelegate.swift
+++ Launch/AppDelegate.swift
@@ -13,5 +13,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey : Any]? = nil) -> Bool {
+        return true
+    }
+    
+    func applicationWillEnterForeground(_ application: UIApplication) {
+    }
+    
+    func applicationWillTerminate(_ application: UIApplication) {
+    }
 }
 

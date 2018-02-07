8df9128fddc1ff1eb64c47c4fa1702a075e5cddf
super-amazing-joey-boy <joseph.slomowitz@gmail.com>
2018-02-07 16:40:46 +1100

    Initial Commit

---
+++ Launch/AppDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.swift
+//  Launch
+//
+//  Created by Dulio Denis on 11/18/17.
+//  Copyright © 2017 ddApps. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+}
+
---
+++ Launch/ViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ViewController.swift
+//  Launch
+//
+//  Created by Dulio Denis on 11/18/17.
+//  Copyright © 2017 ddApps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+}
+
